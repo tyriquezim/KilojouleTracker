1f8835c91fb6e38c1f9d5ae0a9deffa65e4440cf
tyriquezim <zimbizi.tyrique@gmail.com>
2025-11-09 14:28:02 +0800

    Updated the =manifest and created the files.xml to store meal photos

--- app/src/main/java/com/android/personal/kilojouletracker/model/MealPhoto.kt
+++ app/src/main/java/com/android/personal/kilojouletracker/model/MealPhoto.kt
@@ -3,6 +3,6 @@ package com.android.personal.kilojouletracker.model
 import androidx.room.Entity
 
 @Entity
-class MealPhoto(val mealOwnerId: String, val bitmapByteArray: ByteArray)
+class MealPhoto(val mealOwnerId: String, val photoFileName: String?)
 {
 }
